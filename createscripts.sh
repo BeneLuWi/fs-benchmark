bcf7e6b8637266e2f9ebaac5047f1403677a6787
thefeinkoster <s4dakaub@uni-trier.de>
2020-02-20 16:02:36 +0100

    changed repo structure

--- createscripts.sh
+++ createscripts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-BTRFSSCRIPTS=$(pwd)/btrfs/
-EXT4SCRIPTS=$(pwd)/ext4/
+BTRFSSCRIPTS=$(pwd)/scripts/btrfs/
+EXT4SCRIPTS=$(pwd)/scripts/ext4/
 
-BASESCRIPTS=$(pwd)/basescripts/*
+BASESCRIPTS=$(pwd)/scripts/basescripts/*
 
 echo "Processing btrfs scripts"
 for f in $BASESCRIPTS
@@ -13,7 +13,7 @@ do
 		cp $f $BTRFSSCRIPTS
 		NEWFILE=$BTRFSSCRIPTS/$(basename $f)-$i
 		mv $BTRFSSCRIPTS/$(basename $f) $NEWFILE
-		sed -i 's/set $dir=\/tmp/set $dir=\.\/mount\/btrfs/' $NEWFILE
+		sed -i "s?dir=\/tmp?dir=$(pwd)\/mount\/btrfs?" $NEWFILE
 		sed -i "s/nfiles=.*/nfiles=$i/" $NEWFILE
 	done
 done
@@ -27,7 +27,7 @@ do
 		cp $f $EXT4SCRIPTS
 		NEWFILE=$EXT4SCRIPTS/$(basename $f)-$i
 		mv $EXT4SCRIPTS/$(basename $f) $NEWFILE
-		sed -i 's/set $dir=\/tmp/set $dir=\.\/mount\/ext4/' $NEWFILE
+		sed -i "s?dir=\/tmp?dir=$(pwd)\/mount\/ext4?" $NEWFILE
 		sed -i "s/nfiles=.*/nfiles=$i/" $NEWFILE
 	done
 done
