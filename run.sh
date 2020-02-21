40165fe176204a6580c72f214b581c337aaf8873
thefeinkoster <s4dakaub@uni-trier.de>
2020-02-21 13:55:06 +0100

    edited

--- run.sh
+++ run.sh
@@ -3,33 +3,39 @@ BTRFSSCRIPTS=$(pwd)/scripts/btrfs/*
 EXT4SCRIPTS=$(pwd)/scripts/ext4/*
 
 echo "Processing btrfs scripts"
-for f in $BTRFSSCRIPTS
-do 
-	echo "Processing $f"
-	RESULTFILE=$(pwd)/results/btrfs/$(basename $f).txt
+for i in {1..5}
+do
+	for f in $BTRFSSCRIPTS
+	do 
+		echo "Processing $f"
+		RESULTFILE=$(pwd)/results/btrfs/$(basename $f).txt
+		
 
-	rm -r $(pwd)/mount/btrfs/*
+		filebench -f $f | grep -n 'IO Summary'  >> $RESULTFILE
 
-	filebench -f $f | grep -n 'IO Summary'  >> $RESULTFILE
+		
+		sed -i -E 's/(ops|ops\/s|rd\/wr|mb\/s|ms\/op|.*IO Summary: )//g' $RESULTFILE
+		sed -i -E 's/ +/,/g' $RESULTFILE
 
-	
-	sed -i -E 's/(ops|ops\/s|rd\/wr|mb\/s|ms\/op|.*IO Summary: )//g' $RESULTFILE
-	sed -i -E 's/ +/,/g' $RESULTFILE
-
+	done
 done
+rm -r $(pwd)/mount/btrfs/*
 
 echo "Processing ext4 scripts"
-for f in $EXT4SCRIPTS
-do 
-	echo "Processing $f"
-	RESULTFILE=$(pwd)/results/ext4/$(basename $f).txt
+for i in {1..5}
+do
 
-	rm -r $(pwd)/mount/ext4/*
-	
-	filebench -f $f | grep -n 'IO Summary'  >> $RESULTFILE
+	for f in $EXT4SCRIPTS
+	do 
+		echo "Processing $f"
+		RESULTFILE=$(pwd)/results/ext4/$(basename $f).txt	
 		
-	sed -i -E 's/(ops|ops\/s|rd\/wr|mb\/s|ms\/op|.*IO Summary: )//g' $RESULTFILE
-	sed -i -E 's/ +/,/g' $RESULTFILE
+		filebench -f $f | grep -n 'IO Summary'  >> $RESULTFILE
+			
+		sed -i -E 's/(ops|ops\/s|rd\/wr|mb\/s|ms\/op|.*IO Summary: )//g' $RESULTFILE
+		sed -i -E 's/ +/,/g' $RESULTFILE
 
+	done
 done
+rm -r $(pwd)/mount/ext4/*
 
