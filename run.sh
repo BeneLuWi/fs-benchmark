2221265ddb440836cdc178fb5d4bea69a188ebad
frosty <s4dakaub@uni-trier.de>
2020-02-23 18:06:53 +0100

    another change

--- run.sh
+++ run.sh
@@ -15,49 +15,34 @@ if [ -z "$2" ]
 
 echo "Bechmarking a $1 with $2 replications"
 
-
-BTRFSSCRIPTS=$(pwd)/scripts/btrfs/*
-EXT4SCRIPTS=$(pwd)/scripts/ext4/*
-
-
 RESULTFILE=$(pwd)/results/result$(date +"%d_%m_%H_%M").csv
+declare -a arr=("btrfs" "ext4")
 
-echo "Processing btrfs scripts"
-for i in {1..$2}
+
+for filesystem in "${arr[@]}"
 do
-	for f in $BTRFSSCRIPTS
-	do 
-		echo "Processing $f"
+	SCRIPTS="$(pwd)/scripts/$filesystem/*"
+##TODO is this the right dir? -> f in scripts doesn't find any files?
 	
+	for i in {1..$2}
+	do
+		for f in $SCRIPTS
+		do 
+			echo "Processing $f"
 		
-		filebench -f $f | grep -n 'IO Summary'  >> $RESULTFILE
-
-		
-		sed -i -E "s/(ops|ops\/s|rd\/wr|mb\/s|ms\/op)//g" $RESULTFILE
-		sed -i -E "s/(.*IO Summary: )/$1,ext4,$(basename $f),/g" $RESULTFILE
-		sed -i -E 's/ +/,/g' $RESULTFILE
-		sed -i -E 's/,,/,/g' $RESULTFILE
-
-	done
-done
-rm -r $(pwd)/mount/btrfs/*
-
-echo "Processing ext4 scripts"
-for i in {1..$2}
-do
-
-	for f in $EXT4SCRIPTS
-	do 
-		echo "Processing $f"
-		
-		filebench -f $f | grep -n 'IO Summary'  >> $RESULTFILE
 			
-		sed -i -E "s/(ops|ops\/s|rd\/wr|mb\/s|ms\/op)//g" $RESULTFILE
-		sed -i -E "s/(.*IO Summary: )/$1,btrfs,$(basename $f),/g" $RESULTFILE
-		sed -i -E 's/ +/,/g' $RESULTFILE
-		sed -i -E 's/,,/,/g' $RESULTFILE
+			filebench -f $f | grep -n 'IO Summary'  >> $RESULTFILE
 
+			
+			sed -i -E "s/(ops|ops\/s|rd\/wr|mb\/s|ms\/op)//g" $RESULTFILE
+			sed -i -E "s/(.*IO Summary: )/$1,$filesystem,$(basename $f),/g" $RESULTFILE
+			sed -i -E 's/ +/,/g' $RESULTFILE
+			sed -i -E 's/,,/,/g' $RESULTFILE
+
+		done
 	done
-done
-rm -r $(pwd)/mount/ext4/*
+	rm -r $(pwd)/mount/$filesystem/*
 
+
+
+done
